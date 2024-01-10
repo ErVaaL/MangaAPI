e59a8808e99cbc3e22fd647583ee712759c03f4c
ErVaaL <130446401+ErVaaL@users.noreply.github.com>
2024-01-10 21:31:52 +0100

    Prepared web-api controller and service

---
+++ manga-web-api/src/main/java/com/manga/webapi/services/IMangaService.java
@@ -0,0 +1,4 @@
+package com.manga.webapi.services;
+
+public interface IMangaService {
+}
---
+++ manga-web-api/src/main/java/com/manga/webapi/services/MangaService.java
@@ -0,0 +1,9 @@
+package com.manga.webapi.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MangaService implements IMangaService{
+}
