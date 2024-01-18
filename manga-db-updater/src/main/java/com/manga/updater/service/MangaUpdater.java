fb397f48b9f8269bbf858acd1c9f4a0b3cca07e3
ErVaaL <130446401+ErVaaL@users.noreply.github.com>
2024-01-18 23:18:33 +0100

    Repaired updater
    Creating relations now works for all entities and doesn't throw exceptions
    
    Also adjusted updater for Optional<TEntity> methods

--- manga-db-updater/src/main/java/com/manga/updater/service/MangaUpdater.java
+++ manga-db-updater/src/main/java/com/manga/updater/service/MangaUpdater.java
@@ -2,16 +2,15 @@ package com.manga.updater.service;
 
 import com.manga.client.IMangaClient;
 import com.manga.client.IMangaDictionariesClient;
+import com.manga.client.contract.RelationshipDto;
 import com.manga.model.Genre;
-import com.manga.model.Manga;
 import com.manga.repositories.ICatalogData;
 import com.manga.updater.mappers.ICatalogMappers;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,52 +25,49 @@ public class MangaUpdater implements IUpdateManga{
     private final IMangaDictionariesClient dictionariesClient;
     private final ICatalogMappers mapper;
     @Override
+    @Transactional
     public void updateByYear(int year) {
-        updateDictionaries();
+        if(data.getGenres().findAll().isEmpty()){
+            updateDictionaries();
+        }
         var mangaResultList = mangaClient.getMangaListByYear(year);
-        var mangaEntities = mangaResultList.getData().stream().map(dto->mapper.getMangaMapper().map(dto)).toList();
+        var mangaEntitiesToSave = mangaResultList.getData().stream().map(dto->mapper.getMangaMapper().map(dto)).toList();
+        data.getMangas().saveAll(mangaEntitiesToSave.stream()
+                .filter(Predicate.not(entity->data.getMangas().findBySourceId(entity.getSourceId()).isPresent()))
+                .toList());
 
-        var authors = data.getAuthors().findAll();
-        var authorsSourceIds = authors.stream().map(author->author.getSourceId()).toList();
-        var mangaSourceIds = data.getMangas().findAll().stream().map(Manga::getSourceId).toList();
-        var entitiesToSave = mangaEntities.stream().filter(Predicate.not(entity-> mangaSourceIds.contains(entity.getSourceId())))
-                .toList();
-        entitiesToSave.forEach(entity->{
+        mangaEntitiesToSave.forEach(entity->{
             var matchingMangaDto = mangaResultList.getData().stream()
                     .filter(mangaDto->mangaDto.getId().equals(entity.getSourceId()))
                     .findFirst().get();
-            var mangaRelationships = matchingMangaDto.getRelationships();
             var mangaGenresDtos = matchingMangaDto.getAttributes().getTags().stream().filter(tagDto->tagDto.getAttributes().getGroup().equals("genre")).toList();
-            var mangaGenres = mangaGenresDtos.stream().map(tagDto->mapper.getGenreMapper().map(tagDto)).toList();
-            var genres = data.getGenres().findAll();
-            if(!mangaRelationships.isEmpty()){
-                var authorRelationship = mangaRelationships.stream()
-                        .filter(relationshipDto->relationshipDto.getType().equals("author"))
-                        .findFirst();
-                if(authorRelationship.isPresent()){
-                    var authorDto = mangaClient.getAuthorById(authorRelationship.get().getId()).getData();
-                    var authorEntity = mapper.getAuthorMapper().map(authorDto);
-                    if(!authorsSourceIds.contains(authorEntity.getSourceId())) data.getAuthors().save(authorEntity);
-                    entity.setAuthor(authorEntity);
-                }else {
-                    entity.setAuthor(null);
-                }
+
+            var relationshipDto = (RelationshipDto) matchingMangaDto.getRelationships().stream()
+                    .filter(dto->dto.getType().equals("author"))
+                    .findFirst().get();
+            var authorEntity = data.getAuthors().findBySourceId(relationshipDto.getId());
+            if(authorEntity.isPresent()){
+                entity.setAuthor(authorEntity.get());
+            }else {
+                var authorDto = mangaClient.getAuthorById(relationshipDto.getId()).getData();
+                var author = mapper.getAuthorMapper().map(authorDto);
+                data.getAuthors().save(author);
+                entity.setAuthor(author);
             }
-            if(!mangaSourceIds.contains(entity.getSourceId())){
-                data.getMangas().save(entity);
-            }
+
             mangaGenresDtos.forEach(tagDto->{
                 var genreEntity = data.getGenres().findBySourceId(tagDto.getId());
-                if(genreEntity == null){
+                genreEntity.ifPresent(genre -> {
+                    if(!entity.getGenres().contains(genre)){
+                        entity.getGenres().add(genre);
+                    }
+                });
+                if(genreEntity.isEmpty()){
                     data.getGenres().save(mapper.getGenreMapper().map(tagDto));
-                    entity.getGenres().add(data.getGenres().findBySourceId(tagDto.getId()));
-                }
-                if(!entity.getGenres().contains(genreEntity)){
-                    entity.getGenres().add(genreEntity);
+                    entity.getGenres().add(mapper.getGenreMapper().map(tagDto));
                 }
             });
-
-        });
+    });
     }
     private void updateDictionaries() {
         var tags = dictionariesClient.getTagsDtos();
