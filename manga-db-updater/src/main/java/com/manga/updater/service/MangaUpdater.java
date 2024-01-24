bbf25c1afac84c6f06a352f90bfac9533b21ff65
ErVaaL <130446401+ErVaaL@users.noreply.github.com>
2024-01-24 16:32:12 +0100

    Extracted methods in updater

--- manga-db-updater/src/main/java/com/manga/updater/service/MangaUpdater.java
+++ manga-db-updater/src/main/java/com/manga/updater/service/MangaUpdater.java
@@ -3,7 +3,13 @@ package com.manga.updater.service;
 import com.manga.client.IMangaClient;
 import com.manga.client.IMangaDictionariesClient;
 import com.manga.client.contract.RelationshipDto;
+import com.manga.client.contract.authorDtos.AuthorDto;
+import com.manga.client.contract.dictionaries.TagDto;
+import com.manga.client.contract.mangaDtos.MangaDto;
+import com.manga.client.contract.mangaDtos.MangaListResultDto;
+import com.manga.model.Author;
 import com.manga.model.Genre;
+import com.manga.model.Manga;
 import com.manga.repositories.ICatalogData;
 import com.manga.updater.mappers.ICatalogMappers;
 import jakarta.transaction.Transactional;
@@ -13,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -27,50 +34,74 @@ public class MangaUpdater implements IUpdateManga{
     @Override
     @Transactional
     public void updateByYear(int year) {
-        if(data.getGenres().findAll().isEmpty()){
-            updateDictionaries();
+
+        if(data.getGenres().findAll().isEmpty()) updateDictionaries();
+
+        MangaListResultDto mangaResultList = mangaClient.getMangaListByYear(year);
+
+        List<Manga> mangaEntitiesToSave = mangaResultList.getData()
+                .stream()
+                .map(dto->mapper.getMangaMapper().map(dto)).toList();
+
+        saveMangaEntities(mangaEntitiesToSave);
+
+        mangaEntitiesToSave.forEach(entity->{
+            MangaDto matchingMangaDto = mangaResultList.getData().stream()
+                    .filter(mangaDto->mangaDto.getId().equals(entity.getSourceId()))
+                    .findFirst().get();
+
+            saveAuthorAndRelation(entity, matchingMangaDto);
+            saveMangaGenreRelation(entity, matchingMangaDto);
+        });
+    }
+
+    private void saveMangaGenreRelation(Manga entity, MangaDto matchingMangaDto) {
+        List<TagDto> mangaGenresDtos = matchingMangaDto.getAttributes()
+                .getTags()
+                .stream()
+                .filter(tagDto->tagDto.getAttributes()
+                        .getGroup()
+                        .equals("genre"))
+                .toList();
+
+        mangaGenresDtos.forEach(tagDto->{
+            Optional<Genre> genreEntity = data.getGenres().findBySourceId(tagDto.getId());
+            genreEntity.ifPresent(genre -> {
+                if(!entity.getGenres().contains(genre)){
+                    entity.getGenres().add(genre);
+                }
+            });
+            if(genreEntity.isEmpty()){
+                data.getGenres().save(mapper.getGenreMapper().map(tagDto));
+                entity.getGenres().add(mapper.getGenreMapper().map(tagDto));
+            }
+        });
+    }
+
+    private void saveAuthorAndRelation(Manga entity, MangaDto matchingMangaDto) {
+        RelationshipDto relationshipDto = matchingMangaDto.getRelationships()
+                .stream()
+                .filter(dto->dto.getType().equals("author"))
+                .findFirst().get();
+        Optional<Author> authorEntity = data.getAuthors().findBySourceId(relationshipDto.getId());
+        if(authorEntity.isPresent()){
+            entity.setAuthor(authorEntity.get());
+        }else {
+            AuthorDto authorDto = mangaClient.getAuthorById(relationshipDto.getId()).getData();
+            Author author = mapper.getAuthorMapper().map(authorDto);
+            data.getAuthors().save(author);
+            entity.setAuthor(author);
         }
-        var mangaResultList = mangaClient.getMangaListByYear(year);
-        var mangaEntitiesToSave = mangaResultList.getData().stream().map(dto->mapper.getMangaMapper().map(dto)).toList();
+    }
+
+    private void saveMangaEntities(List<Manga> mangaEntitiesToSave) {
         data.getMangas().saveAll(mangaEntitiesToSave.stream()
                 .filter(Predicate.not(entity->data.getMangas().findBySourceId(entity.getSourceId()).isPresent()))
                 .toList());
-
-        mangaEntitiesToSave.forEach(entity->{
-            var matchingMangaDto = mangaResultList.getData().stream()
-                    .filter(mangaDto->mangaDto.getId().equals(entity.getSourceId()))
-                    .findFirst().get();
-            var mangaGenresDtos = matchingMangaDto.getAttributes().getTags().stream().filter(tagDto->tagDto.getAttributes().getGroup().equals("genre")).toList();
-
-            var relationshipDto = (RelationshipDto) matchingMangaDto.getRelationships().stream()
-                    .filter(dto->dto.getType().equals("author"))
-                    .findFirst().get();
-            var authorEntity = data.getAuthors().findBySourceId(relationshipDto.getId());
-            if(authorEntity.isPresent()){
-                entity.setAuthor(authorEntity.get());
-            }else {
-                var authorDto = mangaClient.getAuthorById(relationshipDto.getId()).getData();
-                var author = mapper.getAuthorMapper().map(authorDto);
-                data.getAuthors().save(author);
-                entity.setAuthor(author);
-            }
-
-            mangaGenresDtos.forEach(tagDto->{
-                var genreEntity = data.getGenres().findBySourceId(tagDto.getId());
-                genreEntity.ifPresent(genre -> {
-                    if(!entity.getGenres().contains(genre)){
-                        entity.getGenres().add(genre);
-                    }
-                });
-                if(genreEntity.isEmpty()){
-                    data.getGenres().save(mapper.getGenreMapper().map(tagDto));
-                    entity.getGenres().add(mapper.getGenreMapper().map(tagDto));
-                }
-            });
-    });
     }
+
     private void updateDictionaries() {
-        var tags = dictionariesClient.getTagsDtos();
+        List<TagDto> tags = dictionariesClient.getTagsDtos();
         Set<Genre> existingGenres = new HashSet<>(data.getGenres().findAll());
         List<Genre> genresFromTags = dictionariesClient.getTagsDtos().stream()
                 .filter(tagDto -> tagDto.getAttributes().getGroup().equals("genre"))
